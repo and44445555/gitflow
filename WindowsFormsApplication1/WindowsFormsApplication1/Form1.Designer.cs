0d4c69bd664ec8cbce2bd8be79ff20f3b5c5c5b8
and44445555 <9d994@list.ru>
2016-12-05 22:51:32 +0300

    Commit 3 (Develop)

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -45,8 +45,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 48);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Сложение";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -54,7 +55,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(129, 48);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Вычитание";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -63,7 +64,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(129, 48);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Умножение";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -72,7 +73,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(129, 48);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "Деление";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -81,7 +82,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(129, 48);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "В степень";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -90,7 +91,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(129, 48);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "Корень";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // textBox1
